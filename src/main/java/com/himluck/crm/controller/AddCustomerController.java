5022bde2dee6969e33898f0c476e2b6a3f2eecb7
HimalayaKharate <kharatehimalaya@gmail.com>
2025-02-26 22:52:56 +0530

    Added a way for the salesman to put customer ticket and allowed by admin assigned to user.

--- src/main/java/com/himluck/crm/controller/AddCustomerController.java
+++ src/main/java/com/himluck/crm/controller/AddCustomerController.java
@@ -1,12 +1,38 @@
 package com.himluck.crm.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.himluck.crm.model.AddCustomerTicket;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.himluck.crm.service.AddCustomerService;
+
+import java.util.List;
+import java.util.UUID;
+
 @RestController
+@RequestMapping("/crm-api/v1")
 public class AddCustomerController {
     private AddCustomerService service;
 
     public AddCustomerController(AddCustomerService service){
         this.service = service;
+    }
+
+    @GetMapping({"/admin/customer-add", "/sales/customer-add"})
+    public ResponseEntity<List<AddCustomerTicket>> getAllTickets(){
+        List<AddCustomerTicket> list = service.getTickets();
+        if(list.isEmpty()) return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping( "/sales/customer-add")
+    public ResponseEntity<AddCustomerTicket> postTicket(@RequestBody AddCustomerTicket ticket){
+        service.addTicket(ticket);
+        return ResponseEntity.ok(ticket);
+    }
+
+    @PutMapping("/admin/customer-add/allow/{a}")
+    public void allowThem(@PathVariable UUID id, @RequestBody List<UUID> uuids){
+        service.allowTickets(uuids, id);
     }
 }
--- src/main/java/com/himluck/crm/service/AddCustomerService.java
+++ src/main/java/com/himluck/crm/service/AddCustomerService.java
@@ -1,17 +1,56 @@
 package com.himluck.crm.service;
 
 
+import com.himluck.crm.model.AddCustomerTicket;
+import com.himluck.crm.model.Customer;
 import com.himluck.crm.repository.AddCustomerRepository;
+import com.himluck.crm.repository.CustomerRepository;
+import com.himluck.crm.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AddCustomerService {
     AddCustomerRepository repository;
+    UserRepository repo;
+    CustomerRepository customerRepository;
 
-    public AddCustomerService(AddCustomerRepository repo){
+    public AddCustomerService(AddCustomerRepository repo, UserRepository repo1, CustomerRepository repository){
         this.repository = repo;
+        this.repo = repo1;
+        this.customerRepository = repository;
     }
 
 
+    public List<AddCustomerTicket> getTickets() {
+        return repository.findAll();
+    }
 
+    public void addTicket(AddCustomerTicket ticket) {
+        repository.save(ticket);
+    }
+
+    public void allowTickets(List<UUID> uuids, UUID id) {
+        for(UUID uuid : uuids){
+            var t =  repository.findById(id).orElse(null);
+            if(t == null)  return;
+            Customer customer = new Customer();
+            customer.setName(t.getName());
+            customer.setAddress(t.getAddress());
+            customer.setEmail(t.getEmail());
+            customer.setCity(t.getCity());
+            customer.setCountry(t.getCountry());
+            customer.setAssignedTo(repo.findById(id).orElse(null));
+            customer.setCompanyName(t.getCompanyName());
+            customer.setIndustry(t.getIndustry());
+            customer.setPhone(t.getPhone());
+            customer.setState(t.getState());
+            customer.setZipCode(t.getZipCode());
+            customerRepository.save(customer);
+            t.setAssigned(true);
+            repository.save(t);
+        }
+    }
 }
